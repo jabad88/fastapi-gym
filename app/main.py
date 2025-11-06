138e60a976336be8f9644fd804cfa6e28798daff
jabad88 <abadiesjosh@gmail.com>
2025-11-06 15:29:54 -0800

    upload to txt file to s3

--- app/main.py
+++ app/main.py
@@ -3,9 +3,13 @@ from schemas import PostExercise
 from models import Exercise
 from db import engine, Base, get_db
 from sqlalchemy.orm import Session
-
+from dotenv import load_dotenv
+import os
+import boto3
 
 app = FastAPI()
+
+load_dotenv()
 
 Base.metadata.create_all(engine)
 
@@ -25,3 +29,23 @@ async def add_exercise(post:PostExercise, db: Session = Depends(get_db)):
     db.refresh(new_exercise)
 
     return new_exercise
+
+
+@app.post("/upload")
+async def upload_membership_info():
+
+
+    s3_client = boto3.client(
+        service_name='s3',
+        region_name=os.getenv("AWS_REGION"),
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_KEY")
+    )
+    
+    response = s3_client.upload_file("/Users/josh/Documents/fastapi-gym/membership_info.txt", os.getenv("AWS_S3_BUCKET_NAME"),"membership_info1.txt")
+
+    print(f'upload_log_to_aws response: {response}')
+
+    return True
+
+
